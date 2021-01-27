c5582fb4560ea6c646bb05a861144f22b70e9a36
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2021-01-27 23:39:56 +0800

    Linking sound files to components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { DrumPad } from "./components/DrumPad";
+import clap from "./audio/clap.mp3";
+import closedHh from "./audio/closed-hh.mp3";
+import kickHat from "./audio/kick-hat.mp3";
+import kick from "./audio/kick.mp3";
+import openHh from "./audio/open-hh.mp3";
+import punchyKick from "./audio/punchy-kick.mp3";
+import shaker from "./audio/shaker.mp3";
+import sideStick from "./audio/side-stick.mp3";
+import snare from "./audio/snare.mp3";
 
 export const App = () => {
   return (
     <div id="drum-machine">
       <div id="display"></div>
-      <DrumPad id="clap">Q</DrumPad>
-      <DrumPad id="closed-hh">W</DrumPad>
-      <DrumPad id="kick-hat">E</DrumPad>
-      <DrumPad id="kick">A</DrumPad>
-      <DrumPad id="open-hh">S</DrumPad>
-      <DrumPad id="punchy-kick">D</DrumPad>
-      <DrumPad id="shaker">Z</DrumPad>
-      <DrumPad id="side-stick">X</DrumPad>
-      <DrumPad id="snare">C</DrumPad>
+      <DrumPad id="clap" audio={clap} playKey="Q" />
+      <DrumPad id="closed-hh" audio={closedHh} playKey="W" />
+      <DrumPad id="kick-hat" audio={kickHat} playKey="E" />
+      <DrumPad id="kick" audio={kick} playKey="A" />
+      <DrumPad id="open-hh" audio={openHh} playKey="S" />
+      <DrumPad id="punchy-kick" audio={punchyKick} playKey="D" />
+      <DrumPad id="shaker" audio={shaker} playKey="Z" />
+      <DrumPad id="side-stick" audio={sideStick} playKey="X" />
+      <DrumPad id="snare" audio={snare} playKey="C" />
     </div>
   );
 };
--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export const DrumPad = ({ id, children }) => {
+export const DrumPad = ({ id, audio, playKey }) => {
   return (
     <button id={id} className="drum-pad">
-      {children}
+      {playKey}
+      <audio id={playKey} className="clip" src={audio}></audio>
     </button>
   );
 };
